fb34bfa852a6e589ac894c52feeeb17410acee6d
Renzo Damian <renzo.damian.go@gmail.com>
2024-04-08 18:45:30 -0500

    change to english

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="flex justify-center items-center h-screen w-screen flex-col gap-4 px-6 overflow-hidden relative">
           <h1 className=" text-4xl md:text-6xl md:w-[600px] text-center  animate-appear z-10">
             <span className="flex justify-center items-center gap-2 md:gap-3">
-              Hola{" "}
+              Hi{" "}
               <Image
                 className="h-[40px] md:h-[60px] md:w-[60px] animate-wiggle"
                 src={"/img/waving_hand.webp"}
@@ -16,8 +16,8 @@ export default function Home() {
                 alt="Hello"
               />
             </span>
-            Estoy creando un portfolio{" "}
-            <span className="font-semibold">diferente</span>.
+            I'm building my{" "}
+            <span className="font-semibold">portfolio</span>.
           </h1>
 
           <div className="absolute top-0 left-0 text-4xl md:text-7xl font-bold opacity-10 text-justify h-screen w-screen overflow-hidden z-1 select-none">
